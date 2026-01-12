711225967201e99fc6f1f6687ef1ddc07bbc207d
felizemunk <49683992+felizemunk@users.noreply.github.com>
2026-01-12 15:35:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,6 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-'''
 ingredient_list = st.multiselect(
     "Choose up to 5 ingredients",
     my_dataframe,
@@ -47,4 +44,8 @@ if ingredient_list:
     if timne_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
-'''
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# st.text(smoothiefroot_response.json())
+sf_df = st.datafrane(data=smoothiefroot_response.json(), use_container_width=True)
+
